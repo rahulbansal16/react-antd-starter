8ddd019c2ba3544df8195dcc9b2b865f4a4d0848
Rahul Bansal <bansal.rahul14@gmail.com>
2023-06-13 11:45:49 +0530

    Antd with 100% height

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
+import {Layout, Row, Col} from "antd"
 import './App.css';
+const { Header, Content } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout style={{
+      height:'100%'
+    }}>
+      <Header>
+        Short Editor
+      </Header>
+      <Content style={{
+        padding: "0 50px",
+        height:'100%'
+      }}>
+        <Row>
+          <Col span={12}>
+            This is left side
+          </Col>
+
+          <Col span={12}>
+          </Col>
+        </Row>
+      </Content>
+    </Layout>
   );
 }
 
